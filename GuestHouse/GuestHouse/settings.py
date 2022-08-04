ac5ef98766db3f8fadcdd13a3316fc1cccf0e386
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-08-04 18:04:45 +0530

    confirm

--- GuestHouse/GuestHouse/settings.py
+++ GuestHouse/GuestHouse/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MyUser.apps.MyuserConfig'
+    'MyUser.apps.MyuserConfig',
+    'MyAdmin.apps.MyadminConfig',
 ]
 
 MIDDLEWARE = [
