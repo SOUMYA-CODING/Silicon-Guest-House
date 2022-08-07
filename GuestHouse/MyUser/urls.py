3342efbba84a4b2cae53cf2a132818f42cce045b
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-08-07 19:07:56 +0530

    confirm

--- GuestHouse/MyUser/urls.py
+++ GuestHouse/MyUser/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePage, name='HomePage'),
     path('booking/', views.BookingPage, name='BookingPage'),
+    path('login/', views.LoginPage, name='LoginPage'),
+    path('registration/', views.RegistrationPage, name='RegistrationPage'),
 ]
--- GuestHouse/MyUser/views.py
+++ GuestHouse/MyUser/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render
 def HomePage(request):
     return render(request, 'userSection/index.html')
 
+
 # Booking Page
 def BookingPage(request):
     return render(request, 'bookingSection/booking.html')
+
+
+# Login Page
+def LoginPage(request):
+    return render(request, 'authentication/login.html')
+
+
+# Registration Page
+def RegistrationPage(request):
+    return render(request, 'authentication/registration.html')
