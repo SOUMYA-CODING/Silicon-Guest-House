19c432162c9ca063d5a6e3f81376734e75b8f381
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-08-10 00:47:06 +0530

    confirm

--- GuestHouse/MyAdmin/views.py
+++ GuestHouse/MyAdmin/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from MyBooking.models import Booking
 
 
 # Admin Dashboard Page
 def AdminDashboardPage(request):
+    # User Data
     users = User.objects.all()
+
+    # Booking
+    booking = Booking.objects.all()
+    totalBooking = booking.count()
 
     totalUsers = users.count()
     context = {
         'users': users,
         'totalUsers': totalUsers,
+        'booking': booking,
+        'totalBooking': totalBooking,
     }
     return render(request, 'adminSection/customadmin.html', context)
 
