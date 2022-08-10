d35cdd00af0fdc8b4d4f98da3f69a18d9a9e7583
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-08-10 14:09:38 +0530

    confirm

--- GuestHouse/MyAdmin/views.py
+++ GuestHouse/MyAdmin/views.py
@@ -6,7 +6,7 @@ from MyBooking.models import Booking
 # Admin Dashboard Page
 def AdminDashboardPage(request):
     # User Data
-    users = User.objects.all()
+    users = User.objects.all().filter(is_superuser=0)
 
     # Booking
     booking = Booking.objects.all()
